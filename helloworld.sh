5afe0590859f7284b5fef772e4d9b22b4ae68429
Random9904 <alagunajr@gmail.com>
2023-10-24 15:35:11 -0500

    Added helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  helloworld
+# Author:                       Arturo Laguna
+# Date of latest revision:      10/24/2023
+# Purpose:                      Code Fellows 201 Ops Challenge Class 2
+
+# Declaration of variables
+muhStrang="I am working perfectly!"
+# Declaration of functions
+
+
+# Main
+
+echo "YEYEYE"
+echo $muhStrang
+
+# End
