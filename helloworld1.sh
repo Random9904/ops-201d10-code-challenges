8329cbefbd8476b4366ddadc62a0aeaa42e88aaf
Random9904 <alagunajr@gmail.com>
2023-10-25 15:44:29 -0500

    Added helloworld1

---
+++ helloworld1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:                  helloworld1
+# Author:                       Arturo Laguna
+# Date of latest revision:      10/25/2023
+# Purpose:                      Code Fellows 201 Ops Challenge Class 3
+
+# Declaration of variables
+# Declaration of functions
+print_history() {
+    last
+    printf "\nThis is the login history\n\n"
+}
+
+# Main
+print_history
+echo
+print_history
+echo
+print_history
+# End
